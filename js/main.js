753b288216c465fb8cd0363b1043ca91cc4a5113
Andrew Stead <36166831+andrewsteadcc@users.noreply.github.com>
2019-03-05 13:16:12 +0000

    anchors added

--- js/main.js
+++ js/main.js
@@ -5,4 +5,15 @@
     * Jarallax
     ------------------------------------------------*/
     jarallax(document.querySelectorAll('.jarallax'));
+
+
+    function scrollToAnchor(aid){
+        var aTag = $("a[name='"+ aid +"']");
+        $('html,body').animate({scrollTop: aTag.offset().top},1000);
+    }
+
+    $(".anchor-link").on('click', function() {
+      var location = $(this).data('location');
+       scrollToAnchor(location);
+    });
 })(jQuery);
